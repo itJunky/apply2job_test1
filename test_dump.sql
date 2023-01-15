44293fa889dee10fbdf8495fe001ae4df2a92419
itjunky <itjunky@github.com>
2023-01-15 15:12:17 +0300

    SQL

---
+++ test_dump.sql
@@ -0,0 +1,76 @@
+Enter password: 
+-- MySQL dump 10.13  Distrib 8.0.31, for Linux (x86_64)
+--
+-- Host: localhost    Database: webinar_test
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `first`
+--
+
+DROP TABLE IF EXISTS `first`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `first` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `firstname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `first`
+--
+
+LOCK TABLES `first` WRITE;
+/*!40000 ALTER TABLE `first` DISABLE KEYS */;
+INSERT INTO `first` VALUES (1,'firstname'),(2,'secondname'),(3,'thirdname'),(4,'fourthname'),(5,'fifthname'),(6,'sixthname'),(7,'seventhname'),(8,'eighthname'),(9,'ninthname'),(10,'tenthname');
+/*!40000 ALTER TABLE `first` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `last`
+--
+
+DROP TABLE IF EXISTS `last`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `last` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `lastname` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `last`
+--
+
+LOCK TABLES `last` WRITE;
+/*!40000 ALTER TABLE `last` DISABLE KEYS */;
+INSERT INTO `last` VALUES (1,'fisrtlastov'),(2,'secondlastov'),(3,'thirdlastov'),(4,'fourthlastov'),(5,'fifthlastov'),(6,'sixthlastov'),(7,'seventhlastov'),(8,'eighthlastov'),(9,'ninthlastov'),(10,'tenthlastov');
+/*!40000 ALTER TABLE `last` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-01-15 11:55:04
