519ef9283793cd06317ff70131cc3b219306857d
itjunky <itjunky@github.com>
2023-01-17 16:37:41 +0300

    Add join for SQL request

--- app.py
+++ app.py
@@ -39,10 +39,9 @@ class Last(Base):
 
 
 rnd = randrange(10) 
-slct = select(First).filter_by(id=rnd)
-result = session.execute(slct).first()[0]
+slct = select(First.id, First.firstname, Last.lastname).select_from(First).join(Last, First.id == Last.id).filter_by(id=rnd)
+result = session.execute(slct).first()
 
 host = socket.gethostname()
-
-print(f'{result.id} - {result.firstname} AT {host}')
+print(f'{result[0]} - {result[1]} {result[2]} AT {host}')
 
