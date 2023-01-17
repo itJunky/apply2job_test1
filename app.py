aeec3c911857d60af32ea4794e7edbeaf7a4dbde
itjunky <itjunky@github.com>
2023-01-18 00:44:25 +0300

    Infinity loop for outputting

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 import os
 from random import randrange
 import socket
-
+import time
 
 DB_USER = os.getenv('TEST_DB_USER')
 DB_PASS = os.getenv('TEST_DB_PASS')
@@ -19,7 +19,7 @@ else:
     DB_USER = 'root'
     DB_PASS = 'change-me'
     DB_HOST = 'localhost'
-    DB_NAME = 'webina_test'
+    DB_NAME = 'webinar_test'
 
 Base = declarative_base()
 db_path = 'mysql+pymysql://' + DB_USER + ':' + DB_PASS + '@' + DB_HOST + '/' + DB_NAME
@@ -38,11 +38,18 @@ class Last(Base):
     id = Column(Integer, nullable=False, primary_key=True)
     lastname = Column(String(255))
 
+def check():
+    rnd = randrange(10) 
+    slct = select(First.id, First.firstname, Last.lastname).select_from(First).join(Last, First.id == Last.id).filter_by(id=rnd)
+    result = session.execute(slct).first()
+    
+    host = socket.gethostname()
+    try:
+        print(f'{result[0]} - {result[1]} {result[2]} AT {host}')
+    except Exception as e:
+        print(f'SOME ERROR: {e}')
 
-rnd = randrange(10) 
-slct = select(First.id, First.firstname, Last.lastname).select_from(First).join(Last, First.id == Last.id).filter_by(id=rnd)
-result = session.execute(slct).first()
-
-host = socket.gethostname()
-print(f'{result[0]} - {result[1]} {result[2]} AT {host}')
+while True:
+    check()
+    time.sleep(1)
 
